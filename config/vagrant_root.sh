3995fed88bd536c1c18903c7ca5a0089ab263efe
Austin DeVinney <austin@echoditto.com>
2015-07-02 10:53:05 -0400

    Updates to setup Added node and npm version updates, removed unneeded package.json information

--- config/vagrant_root.sh
+++ config/vagrant_root.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+#uddate apt and get basic tools
 apt-get update
 apt-get install -y vim
 apt-get install -y git
+
+#python
 apt-get install -y python
 apt-get install -y python-virtualenv
+
+#node setup with mongodb
 apt-get install -y mongodb
 apt-get install -y nodejs
 apt-get install -y nodejs-legacy
 apt-get install -y npm
+
+#install grunt via npm
 npm install -g grunt-cli
 npm install -g grunt-init
+
+#node version manager, most recent npm and node
+npm install -g n
+n stable
+npm install -g npm
